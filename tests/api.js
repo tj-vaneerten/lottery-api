5f80a301e9099182477934ad96e8b52b89fe6a6f
tj <tjvaneerten@gmail.com>
2019-12-19 00:25:54 -0500

    adding tests that will fail build

--- tests/api.js
+++ tests/api.js
@@ -2,6 +2,14 @@ import tap from 'tap';
 import clean from 'mongo-clean';
 import startApp from '../src/server';
 
+const gamePattern = {
+	id: /^[a-zA-Z0-9_]+$/,
+	interval: Number,
+	description: String,
+	lastDrawingTime: Date,
+	nextDrawingTime: Date
+}
+
 startApp({
 	address: 'localhost',
 	port: 3000
@@ -28,15 +36,27 @@ startApp({
 // --------------------------------------------------
 
 	tap.test('GET /games', t => {
+		const db = app.mongo.db;
+		const games = db.collection('games');
 
-		app.inject({
+		// insert two games and test that they return
+		games.insert([
+			{ _id: 'powerball', name: 'PowerBall', description: 'Powerball drawing', interval: 3600 },
+			{ _id: 'megamillions', name: 'Mega Millions', description: 'Mega Millions drawing', interval: 1800 }
+		])
+		.then(() => app.inject({
 			method: 'GET',
 			url: '/games'
-		})
+		}))
 		.then(response => {
 			t.strictEqual(response.statusCode, 200);
+
 			const body = JSON.parse(response.body);
 			t.type(body, Array);
+			t.strictEqual(body.length, 2);
+
+			const game = body[0];
+			t.match(game, gamePattern);
 		})
 		.catch(error => t.error(error))
 		.finally(() => t.end());
@@ -46,12 +66,28 @@ startApp({
 // --------------------------------------------------
 
 	tap.test('GET /games/{gameId}', t => {
-		app.inject({
+		const db = app.mongo.db;
+		const games = db.collection('games');
+
+		// insert two games and get one by id
+		games.insert([
+			{ _id: 'powerball', name: 'PowerBall', description: 'Powerball drawing', interval: 3600 },
+			{ _id: 'megamillions', name: 'Mega Millions', description: 'Mega Millions drawing', interval: 1800 }
+		])
+		.then(() => app.inject({
 			method: 'GET',
-			url: '/games'
-		})
+			url: '/games/powerball'
+		}))
 		.then(response => {
-			//t.strictEqual(response.statusCode, 200);
+			t.strictEqual(response.statusCode, 200);
+
+			const body = JSON.parse(response.body);
+			t.type(body, Object);
+			t.hasStrict(body, {
+				game: Object,
+				pastDrawings: Array
+			});
+			t.match(body.game, gamePattern);
 		})
 		.catch(error => t.error(error))
 		.finally(() => t.end());
